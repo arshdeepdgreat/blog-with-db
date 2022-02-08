3bb84e1444e17b8cc0f605562b5eadc19323944b
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-02-08 12:10:52 +0530

    Commit

--- app.js
+++ app.js
@@ -14,46 +14,56 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
+
+const postSchema = {
+
+  title: String,
+
+  content: String
+
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 
 let posts = [];
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.render("home", {
     startingContent: homeStartingContent,
     posts: posts
-    });
+  });
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
-  const post = {
+app.post("/compose", function (req, res) {
+  const post = new Post ({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-
-  posts.push(post);
-
+  });
+  post.save();
   res.redirect("/");
-
 });
 
-app.get("/posts/:postName", function(req, res){
+app.get("/posts/:postName", function (req, res) {
   const requestedTitle = _.lowerCase(req.params.postName);
 
-  posts.forEach(function(post){
+  posts.forEach(function (post) {
     const storedTitle = _.lowerCase(post.title);
 
     if (storedTitle === requestedTitle) {
@@ -66,6 +76,6 @@ app.get("/posts/:postName", function(req, res){
 
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
